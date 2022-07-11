4131ef4d0f4ff700e9bcd6f2b30cc272e9102d8b
korneliusdassi <dassi.ccan@gmail.com>
2022-07-11 22:59:51 +0800

    membuat router

---
+++ controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./koneksi');
+
+exports.index = function (req, res) {
+  response.ok('Apk ku berjalan');
+};
---
+++ router.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function (app) {
+  var jsonku = require('./controller');
+
+  app.route('/').get(jsonku.index);
+};
