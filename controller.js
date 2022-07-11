f2d18d22d78ba918964bdb4df924d512f5628869
korneliusdassi <dassi.ccan@gmail.com>
2022-07-11 23:40:27 +0800

    tampil data berdasarkan id

--- controller.js
+++ controller.js
@@ -12,7 +12,20 @@ exports.index = function (req, res) {
 exports.tampildata = function (req, res) {
   connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
     if (error) {
-      connection.log(error);
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+//tampilkan data mhs berdasarkan id
+
+exports.tampildataid = function (req, res) {
+  let id = req.params.id;
+  connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa= ?', [id], function (error, rows, fileds) {
+    if (error) {
+      console.log(error);
     } else {
       response.ok(rows, res);
     }
--- router.js
+++ router.js
@@ -5,4 +5,5 @@ module.exports = function (app) {
 
   app.route('/').get(jsonku.index);
   app.route('/tampil').get(jsonku.tampildata);
+  app.route('/tampil/:id').get(jsonku.tampildataid);
 };
