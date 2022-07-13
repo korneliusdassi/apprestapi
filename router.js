e3b9f059716ca1740ee576db137a2a2c381ed8ab
korneliusdassi <dassi.ccan@gmail.com>
2022-07-13 19:08:43 +0800

    new commit

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (app) {
-  var jsonku = require('./controller');
+  var jsonku = require('./handler');
 
   app.route('/').get(jsonku.index);
   app.route('/tampil').get(jsonku.tampildata);
